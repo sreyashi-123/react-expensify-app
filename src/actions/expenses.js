bbc2418c5590577d0c492d4960a04fa35b3220be
sreyashi <sreyashi1995@gmail.com>
2020-04-26 18:55:08 +0530

    Store user data in user area

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -8,7 +8,8 @@ export const addExpense = (expense) => ({
 });
 
 export const startAddExpense = (expenseData = {}) => {
-  return (dispatch) => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
     const {
       description = '',
       note = '',
@@ -17,10 +18,10 @@ export const startAddExpense = (expenseData = {}) => {
     } = expenseData;
     const expense = { description, note, amount, createdAt };
 
-    return database.ref('expenses').push(expense).then((ref) => {
+    return database.ref(`users/${uid}/expenses`).push(expense).then((ref) => {
       dispatch(addExpense({
         id: ref.key,
-        ...expense
+        description, note, amount, createdAt//must be...expense
       }));
     });
   };
@@ -33,8 +34,9 @@ export const removeExpense = ({ id } = {}) => ({
 });
 
 export const startRemoveExpense = ({ id } = {}) => {
-  return (dispatch) => {
-    return database.ref(`expenses/${id}`).remove().then(() => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
+    return database.ref(`users/${uid}/expenses/${id}`).remove().then(() => {
       dispatch(removeExpense({ id }));
     });
   };
@@ -48,8 +50,9 @@ export const editExpense = (id, updates) => ({
 });
 
 export const startEditExpense = (id, updates) => {
-  return (dispatch) => {
-    return database.ref(`expenses/${id}`).update(updates).then(() => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
+    return database.ref(`users/${uid}/expenses/${id}`).update(updates).then(() => {
       dispatch(editExpense(id, updates));
     });
   };
@@ -60,20 +63,21 @@ export const setExpenses = (expenses) => ({
   type: 'SET_EXPENSES',
   expenses
 });
+//must include this part in original file.not working here.
+// export const startSetExpenses = () => {
+//   return (dispatch, getState) => {
+//     const uid = getState().auth.uid;
+//     return database.ref(`users/${uid}/expenses`).once('value').then((snapshot) => {
+//       const expenses = [];
 
-export const startSetExpenses = () => {
-  return (dispatch) => {
-    return database.ref('expenses').once('value').then((snapshot) => {
-      const expenses = [];
+//       snapshot.forEach((childSnapshot) => {
+//         expenses.push({
+//           id: childSnapshot.key,
+//           ...childSnapshot.val()
+//         });
+//       });
 
-      snapshot.forEach((childSnapshot) => {
-        expenses.push({
-          id: childSnapshot.key,
-          ...childSnapshot.val()
-        });
-      });
-
-      dispatch(setExpenses(expenses));
-    });
-  };
-};
+//       dispatch(setExpenses(expenses));
+//     });
+//   };
+// };
--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { startSetExpenses } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
+import { startAddExpense } from './actions/expenses';//must be { startSetExpenses } 
+import { login, logout } from './actions/auth';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-import './firebase/firebase';
+import { firebase } from './firebase/firebase';
 
 const store = configureStore();
 
@@ -19,8 +19,31 @@ const jsx = (
   </Provider>
 );
 
+let hasRendered = false;//let can help us reassign it later
+const renderApp = () => {
+  if(!hasRendered){
+    ReactDOM.render(jsx, document.getElementById('app'));
+    hasRendered = true;
+  }
+}; 
+
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-store.dispatch(startSetExpenses()).then(() => {
-  ReactDOM.render(jsx, document.getElementById('app'));
+
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {        
+    store.dispatch(login(user.uid));              //store.dispatch(startSetExpenses()).then(() => { -this must be done in original file. 
+    store.dispatch(startAddExpense()).then(() => {//this line is done for test purpose only to make it working here.
+      renderApp();
+      if (history.location.pathname === '/') {
+        history.push('/dashboard');
+      }
+    });
+    console.log('log in');
+  } else {
+    store.dispatch(logout()); 
+    renderApp();
+    history.push('/');
+  }
 });
